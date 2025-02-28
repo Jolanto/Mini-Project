484c8b1925412e30c6d76a08ede21d04447b597f
Jolanto <69620483+Jolanto@users.noreply.github.com>
2025-02-28 15:02:11 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     outDir: "dist", // Ensure this is correctly set
-    chunkSizeWarningLimit: 2000,
+    chunkSizeWarningLimit: 4000,
   },
   plugins: [react()],
 });
